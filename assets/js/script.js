ec0e213cec5530b00fef6e60a07abbb18cec6663
ConnieMarie <constance.marie.strahan@gmail.com>
2022-08-26 14:49:05 -0500

    new branch

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var apiKey = "+UAPOEz8eS3RqXRJjvFjxA==jfNUZHAlHVThyjIt"
+
+var category = 'music';
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/trivia?category=' + category,
+    headers: { 'X-Api-Key': apiKey},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
