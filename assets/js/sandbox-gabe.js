b525fc38e0b9f035d3c1b834904ca1be5ee29a0f
yiladien@hotmail.com <yiladien@hotmail.com>
2022-08-29 13:51:50 -0500

    fixed button logic

--- assets/js/sandbox-gabe.js
+++ assets/js/sandbox-gabe.js
@@ -22,3 +22,58 @@ $(".inputBtnGroup #clearBtn").click(function () {
   console.log("Clear button clicked");
   $("#apiUrlInput").val("");
 });
+
+// Idea - dynamically populate trivia category
+
+// function runs only on the home page.
+// purpose of the function is to dynamically populate the trivia choices
+$(function () {
+  if ($("body").hasClass(".homepage")) {
+    var triviaCategories = function (repo) {};
+  }
+});
+
+var triviaAPIQuestion =
+  "https://opentdb.com/api.php?amount=1&category=12&type=multiple";
+
+var triviaCategoryUrl = "https://opentdb.com/api_category.php";
+
+document.location.href = newUrl;
+
+
+
+// CSS Ideas
+
+.questionIndicators {
+  display: flex;
+  font-size: 1em;
+  justify-content: space-around;
+  padding-right: 10%;
+  border-style: solid;
+  border-color: white;
+  color: white;
+}
+
+<footer class="footer">
+      <div class="columns">
+        <div class="column is-8">
+          <ul class="questionIndicators">
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+            <li>X</li>
+          </ul>
+        </div>
+        <div class="columns is-4">
+          <div class="is-italic title is-4 has-text-right has-text-white">
+            Made by Team Awesome
+          </div>
+        </div>
+      </div>
+    </footer>
