e4fb7424c65fab4b71028813bc2b5b9db5d251ae
yiladien@hotmail.com <yiladien@hotmail.com>
2022-08-30 05:34:19 -0500

    updates question and choices from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
+var questionData; //need global for click event
 var score = 0;
 var gameQuestionCountLimit = 10;
 var gameQuestionCount = 0;
 var gameCategoryId = "";
-var apiUrl =
+var giphyApiKey = "IqouWAvchaDj6oy5b7niRntKCW50BpKB";
+var giphyApiUrl =
+  "http://api.giphy.com/v1/gifs/search?q=<searchTerm>&api_key=<giphyApiKey>&limit=1";
+var triviaApiUrl =
   "https://opentdb.com/api.php?amount=1&category=<catId>&type=multiple";
 
 var gameCategoryList = [
@@ -32,8 +36,14 @@ if ($("body").hasClass("homepage")) {
 
     //stores the data-catId from the dropdown to add to the API search
     gameCategoryId = $(event.target).attr("data-catId");
-    console.log(gameCategoryId);
-    $("#dropdownText").text(topic);
+
+    if (gameCategoryId) {
+      // when page changes, the javascript variables lose the data
+      // added category to sessionStorage for the trivia page to use in api
+      sessionStorage.setItem("triviaCategory", gameCategoryId);
+      console.log(gameCategoryId);
+      $("#dropdownText").text(topic);
+    }
   });
 
   //Prevents moving forward to play game without a category selected
@@ -51,7 +61,14 @@ if ($("body").hasClass("homepage")) {
 }
 
 //runs event listeners specific to the gamePage
-if ($("body").hasClass("gamePage")) {
+if ($("body").hasClass("triviaPage")) {
+  //loading sessionStorage
+  if (sessionStorage.getItem("triviaCategory")) {
+    // Restore the contents of the text field
+    gameCategoryId = sessionStorage.getItem("triviaCategory");
+    console.log(gameCategoryId);
+  }
+
   $("#startButton").click(function (event) {
     //checks to ensure a category was selected and no undefined
     if (gameCategoryId && gameCategoryId != "") {
@@ -65,29 +82,51 @@ if ($("body").hasClass("gamePage")) {
     }
   });
 
-  $("#choiceButtons").click(function (event) {
-    // add logic to test if a button was pressed
-    // if yes,
-    // store data-choiceId value to a variable
-    // test answer against question variable
-    // if correct
-    //add to score
-    //provide response to user
-    // if incorrect
-    //provide response to user
-    // end with endGame check (if that was the last question to ask)
-    //if total questions asked match the total variable, run the endGame function
-    //else clear screen and run getQuestion again
+  $("#gameArea").on("click", ".choiceButton", function () {
+    var chosenAnswer = $(this).text();
+    console.log(chosenAnswer);
+    console.log(questionData.answer);
+    if (chosenAnswer == questionData.answer) {
+      console.log("Correct");
+      score++;
+      //provide user response
+    } else {
+      console.log("Incorrect");
+      //provide user response
+    }
+
+    // !!!!! still need to disable the answer buttons to prevent multiple clicks until next button is clicked
+
+    // add next question button or finish button if last question
+    //click event will look for .nextQuestion class and call getQuestion();
+    if (gameQuestionCount >= gameQuestionCountLimit) {
+      $("#questionBody").append(
+        "<div class='has-text-centered mt-6'><button class='button is-primary is-large px-6' id='nextQuestion'>Finish</button></div>"
+      );
+    } else {
+      $("#questionBody").append(
+        "<div class='has-text-centered mt-6'><button class='button is-primary is-large px-6' id='nextQuestion'>Next</button></div>"
+      );
+    }
+  });
+
+  $("#gameArea").on("click", "#nextQuestion", function () {
+    getQuestion();
   });
 }
 
 var getQuestion = function () {
-  // gameCategoryId = 23;
-
+  // end with endGame check (if that was the last question to ask)
+  //if total questions asked match the total variable, run the endGame function
+  //else clear screen and run getQuestion again
   if (gameQuestionCount >= gameQuestionCountLimit) {
     endGame();
   } else {
-    var fetchUrl = apiUrl.replace("<catId>", gameCategoryId);
+    gameQuestionCount++;
+    // clearing question area
+    $("#gameArea").html("");
+
+    var fetchUrl = triviaApiUrl.replace("<catId>", gameCategoryId);
     // make a request to the url
     fetch(fetchUrl)
       .then(function (response) {
@@ -96,8 +135,64 @@ var getQuestion = function () {
           response.json().then(function (data) {
             // bulma loading icon class
             // $(":button").toggleClass("is-loading");
-            // console.log(data);
-            displayQuestion(data);
+            console.log(data);
+
+            // create question and answer array in consistent format for a random answer list
+            console.log(data.results[0].question);
+
+            // global object to hold question data
+            questionData = {
+              question: data.results[0].question,
+              answer: data.results[0].correct_answer,
+              choices: [],
+            };
+
+            // temporary array to combine incorrect and correct answers
+            var choiceList = [];
+            // loop to fill array with incorrect answers
+            for (var i = 0; i < data.results[0].incorrect_answers.length; i++) {
+              choiceList.push(data.results[0].incorrect_answers[i]);
+            }
+            // final array push to add correct answer to the end
+            choiceList.push(data.results[0].correct_answer);
+
+            //adding all choices to the questionData object array in random order
+            while (choiceList.length > 0) {
+              // get random number for position in choice array
+              var indexNum = Math.floor(Math.random() * choiceList.length);
+
+              // push matching value in choice array into question object
+              questionData.choices.push(choiceList[indexNum]);
+
+              // set value that was pushed to null in choice array
+              choiceList[indexNum] = null;
+
+              // console.log(indexNum);
+              // console.log(choiceList);
+
+              var lessChoiceArray = [];
+              lessChoiceArray.length = 0;
+              // create a new array but only push values that are not null
+              for (var k = 0; k < choiceList.length; k++) {
+                if (choiceList[k]) {
+                  lessChoiceArray.push(choiceList[k]);
+                }
+              }
+
+              // clearing old array data
+              choiceList.length = 0;
+
+              // adding remaining choices to the choice list
+              choiceList = lessChoiceArray;
+
+              // console.log(choiceList);
+              // console.log(lessChoiceArray);
+            }
+            // questionData.choices = choiceList;
+
+            // console.log(questionData);
+
+            displayQuestion(questionData);
           });
         } else {
           // bulma loading icon class
@@ -114,20 +209,40 @@ var getQuestion = function () {
   }
 };
 
-function displayQuestion(question) {
-  console.log(question);
+function displayQuestion(triviaData) {
+  // console.log(triviaData);
 
-  //
-  // $("#gameArea").html(
-  //   "<div class='columns'><div class='column is-8'></div><div id='choiceButtons' class='column is-4'></div></div>"
-  // );
+  //creating array with all the html with all choice buttons
+  var choiceButtonAppender = [];
+  for (var i = 0; i < 4; i++) {
+    choiceButtonAppender.push(
+      "<button class='button my-5 is-primary is-large is-fullwidth has-text-weight-bold choiceButton'>" +
+        triviaData.choices[i] +
+        "</button>"
+    );
+  }
+  // combining array without delimiters into one html string
+  var choiceButtonEl = choiceButtonAppender.join("");
 
-  // First clear old elements and questions
-  // containder is #choiceButtons
+  // clears and adds in divs for the game area
+  $("#gameArea").html(
+    "<div class='columns'><div id='questionArea' class='column is-8'></div><div id='choiceButtons' class='column is-4'></div></div>"
+  );
 
-  // Add elements with new questions while adding the data attribute to the button elements
-
-  // Add question count number to question header
+  // creates question area elements and slides in from left
+  $("#questionArea")
+    .html(
+      "<article class='message is-primary my-5'><div class='message-header'><p id='questionTitle' class='title is-4 has-text-white'>Question " +
+        (gameQuestionCount + 1) +
+        "</p></div><div id='questionBody' class='message-body is-size-4 has-text-left'>" +
+        triviaData.question +
+        "</div></article>"
+    )
+    .effect("slide", { direction: "right" }, 400, function () {
+      $("#choiceButtons")
+        .html(choiceButtonEl)
+        .effect("slide", { direction: "right" }, 800);
+    });
 }
 
-getQuestion();
+// getQuestion();
